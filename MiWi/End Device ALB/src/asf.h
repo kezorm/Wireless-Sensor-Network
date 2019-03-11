3b8c24f2129e682e7c724e4c7f87d1fd775592b0
Greg Mrozek <greg.mrozek@microchip.com>
2019-03-11 08:08:41 -0500

    Add ADC ASF code

--- MiWi/End Device ALB/src/asf.h
+++ MiWi/End Device ALB/src/asf.h
@@ -52,6 +52,9 @@
  * running the ASF driver selector tool. Any changes will be discarded.
  */
 
+// From module: ADC - Analog-to-Digital Converter (Polled APIs)
+#include <adc.h>
+
 // From module: AT30TSE75X Temperature Sensor
 #include <at30tse75x.h>
 
